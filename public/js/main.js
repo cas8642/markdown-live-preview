f8dc1b53c2c234dc1cb7f35ef2880133040c451d
cas8642 <159821239+cas8642@users.noreply.github.com>
2025-01-07 10:41:04 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let hasEdited = false;
-    let scrollBarSync = false;
+  let hasEdited = false;
+  let scrollBarSync = false;
 
-    const localStorageNamespace = 'com.markdownlivepreview';
-    const localStorageKey = 'last_state';
-    const localStorageScrollBarKey = 'scroll_bar_settings';
-    const confirmationMessage = 'Are you sure you want to reset? Your changes will be lost.';
+  const localStorageNamespace = "com.markdownlivepreview";
+  const localStorageKey = "last_state";
+  const localStorageScrollBarKey = "scroll_bar_settings";
+  const confirmationMessage = "Are you sure you want to reset? Your changes will be lost.";
 
-    // default template
-    const defaultInput = `# Markdown syntax guide
+  // default template
+  const defaultInput = `# Markdown syntax guide
 
 ## Headers
 
@@ -79,198 +79,198 @@ ${"`"}${"`"}${"`"}
 This web site is using ${"`"}markedjs/marked${"`"}.
 `;
 
-    let setupEditor = () => {
-        let editor = ace.edit('editor');
-        editor.getSession().setUseWrapMode(true);
-        editor.setOptions({
-            maxLines: Infinity,
-            indentedSoftWrap: false,
-            fontSize: 14,
-            autoScrollEditorIntoView: true,
-            theme: 'ace/theme/chrome',
-        });
+  let setupEditor = () => {
+    let editor = ace.edit("editor");
+    editor.getSession().setUseWrapMode(true);
+    editor.setOptions({
+      maxLines: Infinity,
+      indentedSoftWrap: false,
+      fontSize: 14,
+      autoScrollEditorIntoView: true,
+      theme: "ace/theme/chrome",
+    });
 
-        var MarkdownMode = ace.require("ace/mode/markdown").Mode;
-        editor.session.setMode(new MarkdownMode());
+    var MarkdownMode = ace.require("ace/mode/markdown").Mode;
+    editor.session.setMode(new MarkdownMode());
 
-        editor.on('change', () => {
-            let changed = editor.getValue() != defaultInput;
-            if (changed) {
-                hasEdited = true;
-            }
-            let value = editor.getValue();
-            convert(value);
-            saveLastContent(value);
-        });
+    editor.on("change", () => {
+      let changed = editor.getValue() != defaultInput;
+      if (changed) {
+        hasEdited = true;
+      }
+      let value = editor.getValue();
+      convert(value);
+      saveLastContent(value);
+    });
 
-        return editor;
+    return editor;
+  };
+
+  // Render markdown text as html
+  let convert = (markdown) => {
+    let options = {
+      headerIds: false,
+      mangle: false,
     };
+    let html = marked.parse(markdown, options);
+    let sanitized = DOMPurify.sanitize(html);
+    document.querySelector("#output").innerHTML = sanitized;
+  };
 
-    // Render markdown text as html
-    let convert = (markdown) => {
-        let options = {
-            headerIds: false,
-            mangle: false
-        };
-        let html = marked.parse(markdown, options);
-        let sanitized = DOMPurify.sanitize(html);
-        document.querySelector('#output').innerHTML = sanitized;
-    };
-
-    // Reset input text
-    let reset = () => {
-        let changed = editor.getValue() != defaultInput;
-        if (hasEdited || changed) {
-            var confirmed = window.confirm(confirmationMessage);
-            if (!confirmed) {
-                return;
-            }
-        }
-        presetValue(defaultInput);
-        document.querySelectorAll('.column').forEach((element) => {
-            element.scrollTo({top: 0});
-        });
-    };
-
-    let presetValue = (value) => {
-        editor.setValue(value);
-        editor.moveCursorTo(0, 0);
-        editor.focus();
-        editor.navigateLineEnd();
-        hasEdited = false;
-    };
-
-    // ----- sync scroll position -----
-
-    let initScrollBarSync = (settings) => {
-        let checkbox = document.querySelector('#sync-scroll-checkbox');
-        checkbox.checked = settings;
-        scrollBarSync = settings;
-
-        checkbox.addEventListener('change', (event) => {
-            let checked = event.currentTarget.checked;
-            scrollBarSync = checked;
-            saveScrollBarSettings(checked);
-        });
-
-        document.querySelector('#edit').addEventListener('scroll', (event) => {
-            if (!scrollBarSync) {
-                return;
-            }
-            let editorElement = event.currentTarget;
-            let ratio = editorElement.scrollTop / (editorElement.scrollHeight - editorElement.clientHeight);
-
-            let previewElement = document.querySelector('#preview');
-            let targetY = (previewElement.scrollHeight - previewElement.clientHeight) * ratio;
-            previewElement.scrollTo(0, targetY);
-        });
-    };
-
-    let enableScrollBarSync = () => {
-        scrollBarSync = true;
-    };
-
-    let disableScrollBarSync = () => {
-        scrollBarSync = false;
-    };
-
-    // ----- clipboard utils -----
-
-    let copyToClipboard = (text, successHandler, errorHandler) => {
-        navigator.clipboard.writeText(text).then(
-            () => {
-                successHandler();
-            },
-
-            () => {
-                errorHandler();
-            }
-        );
-    };
-
-    let notifyCopied = () => {
-        let labelElement = document.querySelector("#copy-button a");
-        labelElement.innerHTML = "Copied!";
-        setTimeout(() => {
-            labelElement.innerHTML = "Copy";
-        }, 1000)
-    };
-
-    // ----- setup -----
-
-    // setup navigation actions
-    let setupResetButton = () => {
-        document.querySelector("#reset-button").addEventListener('click', (event) => {
-            event.preventDefault();
-            reset();
-        });
-    };
-
-    let setupCopyButton = (editor) => {
-        document.querySelector("#copy-button").addEventListener('click', (event) => {
-            event.preventDefault();
-            let value = editor.getValue();
-            copyToClipboard(value, () => {
-                notifyCopied();
-            },
-            () => {
-                // nothing to do
-            });
-        });
-    };
-
-    // ----- local state -----
-
-    let loadLastContent = () => {
-        let lastContent = Storehouse.getItem(localStorageNamespace, localStorageKey);
-        return lastContent;
-    };
-
-    let saveLastContent = (content) => {
-        let expiredAt = new Date(2099, 1, 1);
-        Storehouse.setItem(localStorageNamespace, localStorageKey, content, expiredAt);
-    };
-
-    let loadScrollBarSettings = () => {
-        let lastContent = Storehouse.getItem(localStorageNamespace, localStorageScrollBarKey);
-        return lastContent;
-    };
-
-    let saveScrollBarSettings = (settings) => {
-        let expiredAt = new Date(2099, 1, 1);
-        Storehouse.setItem(localStorageNamespace, localStorageScrollBarKey, settings, expiredAt);
-    };
-
-
-    // ----- entry point -----
-
-    let lastContent = loadLastContent();
-    let editor = setupEditor();
-    if (lastContent) {
-        presetValue(lastContent);
-    } else {
-        presetValue(defaultInput);
+  // Reset input text
+  let reset = () => {
+    let changed = editor.getValue() != defaultInput;
+    if (hasEdited || changed) {
+      var confirmed = window.confirm(confirmationMessage);
+      if (!confirmed) {
+        return;
+      }
     }
-    setupResetButton();
-    setupCopyButton(editor);
+    presetValue(defaultInput);
+    document.querySelectorAll(".column").forEach((element) => {
+      element.scrollTo({ top: 0 });
+    });
+  };
 
-    let scrollBarSettings = loadScrollBarSettings() || false;
-    initScrollBarSync(scrollBarSettings);
-});
+  let presetValue = (value) => {
+    editor.setValue(value);
+    editor.moveCursorTo(0, 0);
+    editor.focus();
+    editor.navigateLineEnd();
+    hasEdited = false;
+  };
 
+  // ----- sync scroll position -----
 
-let isResizing = false;
+  let initScrollBarSync = (settings) => {
+    let checkbox = document.querySelector("#sync-scroll-checkbox");
+    checkbox.checked = settings;
+    scrollBarSync = settings;
 
-const divider = document.getElementById('divider');
-const container = document.getElementById('container');
-const editor = document.getElementById('edit');
-const preview = document.getElementById('preview');
+    checkbox.addEventListener("change", (event) => {
+      let checked = event.currentTarget.checked;
+      scrollBarSync = checked;
+      saveScrollBarSettings(checked);
+    });
 
-divider.addEventListener('mousedown', (e) => {
+    document.querySelector("#edit").addEventListener("scroll", (event) => {
+      if (!scrollBarSync) {
+        return;
+      }
+      let editorElement = event.currentTarget;
+      let ratio = editorElement.scrollTop / (editorElement.scrollHeight - editorElement.clientHeight);
+
+      let previewElement = document.querySelector("#preview");
+      let targetY = (previewElement.scrollHeight - previewElement.clientHeight) * ratio;
+      previewElement.scrollTo(0, targetY);
+    });
+  };
+
+  let enableScrollBarSync = () => {
+    scrollBarSync = true;
+  };
+
+  let disableScrollBarSync = () => {
+    scrollBarSync = false;
+  };
+
+  // ----- clipboard utils -----
+
+  let copyToClipboard = (text, successHandler, errorHandler) => {
+    navigator.clipboard.writeText(text).then(
+      () => {
+        successHandler();
+      },
+
+      () => {
+        errorHandler();
+      }
+    );
+  };
+
+  let notifyCopied = () => {
+    let labelElement = document.querySelector("#copy-button a");
+    labelElement.innerHTML = "Copied!";
+    setTimeout(() => {
+      labelElement.innerHTML = "Copy";
+    }, 1000);
+  };
+
+  // ----- setup -----
+
+  // setup navigation actions
+  let setupResetButton = () => {
+    document.querySelector("#reset-button").addEventListener("click", (event) => {
+      event.preventDefault();
+      reset();
+    });
+  };
+
+  let setupCopyButton = (editor) => {
+    document.querySelector("#copy-button").addEventListener("click", (event) => {
+      event.preventDefault();
+      let value = editor.getValue();
+      copyToClipboard(
+        value,
+        () => {
+          notifyCopied();
+        },
+        () => {
+          // nothing to do
+        }
+      );
+    });
+  };
+
+  // ----- local state -----
+
+  let loadLastContent = () => {
+    let lastContent = Storehouse.getItem(localStorageNamespace, localStorageKey);
+    return lastContent;
+  };
+
+  let saveLastContent = (content) => {
+    let expiredAt = new Date(2099, 1, 1);
+    Storehouse.setItem(localStorageNamespace, localStorageKey, content, expiredAt);
+  };
+
+  let loadScrollBarSettings = () => {
+    let lastContent = Storehouse.getItem(localStorageNamespace, localStorageScrollBarKey);
+    return lastContent;
+  };
+
+  let saveScrollBarSettings = (settings) => {
+    let expiredAt = new Date(2099, 1, 1);
+    Storehouse.setItem(localStorageNamespace, localStorageScrollBarKey, settings, expiredAt);
+  };
+
+  // ----- entry point -----
+
+  let lastContent = loadLastContent();
+  let editor = setupEditor();
+  if (lastContent) {
+    presetValue(lastContent);
+  } else {
+    presetValue(defaultInput);
+  }
+  setupResetButton();
+  setupCopyButton(editor);
+
+  let scrollBarSettings = loadScrollBarSettings() || false;
+  initScrollBarSync(scrollBarSettings);
+
+  let isResizing = false;
+
+  const divider = document.getElementById("divider");
+  const container = document.getElementById("container");
+  const editor = document.getElementById("edit");
+  const preview = document.getElementById("preview");
+
+  divider.addEventListener("mousedown", (e) => {
     isResizing = true;
-});
+  });
 
-document.addEventListener('mousemove', (e) => {
+  document.addEventListener("mousemove", (e) => {
     if (!isResizing) return;
     let containerWidth = container.offsetWidth;
     let offsetX = e.clientX;
@@ -279,8 +279,9 @@ document.addEventListener('mousemove', (e) => {
 
     editor.style.width = `${editorWidthPercentage}%`;
     preview.style.width = `${previewWidthPercentage}%`;
-});
+  });
 
-document.addEventListener('mouseup', () => {
+  document.addEventListener("mouseup", () => {
     isResizing = false;
+  });
 });
